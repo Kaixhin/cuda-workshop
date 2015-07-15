3c58bb235b661ef984ab61437c9de4aec51ea8db
Kaixhin <design@kaixhin.com>
2015-07-15 19:35:03 +0100

    Complete exercise 6, parts 2 and 3

--- thrust.cu
+++ thrust.cu
@@ -1,16 +1,30 @@
-#include <thrust/iterator/counting_iterator.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/reduce.h>
 #include <thrust/functional.h>
-#include <thrust/transform_reduce.h>
-#include <curand_kernel.h>
-#include <iostream>
-#include <iomanip> // we could vary M & N to find the perf sweet spot
-struct estimate_pi : public thrust::unary_function<unsigned int, float> { __device__ float operator()(unsigned int thread_id) { float sum = 0; unsigned int N = 10000; // samples per thread
-  unsigned int seed = thread_id; curandState s; // seed a random number generator
-  curand_init(seed, 0, 0, &s); // take N samples in a quarter circle
-  for(unsigned int i = 0; i < N; ++i) { // draw a sample from the unit square
-    float x = curand_uniform(&s); float y = curand_uniform(&s); // measure distance from the origin
-    float dist = sqrtf(x*x + y*y); // add 1.0f if (u0,u1) is inside the quarter circle
-    if(dist <= 1.0f) sum += 1.0f; } // multiply by 4 to get the area of the whole circle
-  sum *= 4.0f; // divide by N
-  return sum / N; } }; int main(void) { // use 30K independent seeds
-    int M = 30000; float estimate = thrust::transform_reduce( thrust::counting_iterator<int>(0), thrust::counting_iterator<int>(M), estimate_pi(), 0.0f, thrust::plus<float>()); estimate /= M; std::cout << std::setprecision(3); std::cout << "pi is approximately "; std::cout << estimate << std::endl; return 0; }
+#include <thrust/random.h>
+
+int my_rand() {
+  static thrust::default_random_engine rng;
+  static thrust::uniform_int_distribution<int> dist(0, 9999);
+  return dist(rng);
+}
+
+int main() {
+  // Generate random data on host
+  thrust::host_vector<int> h_vec(100);
+  thrust::generate(h_vec.begin(), h_vec.end(), my_rand);
+
+  thrust::device_vector<int> d_vec = h_vec; // Transfer to device
+
+  int init = 0; // Initial value of reduction
+
+  thrust::plus<int> binary_op; // Binary operation used to reduce values
+
+  int sum = thrust::reduce(d_vec.begin(), d_vec.end(), init, binary_op); // Compute sum on device
+
+  std::cout << "Sum is " << sum << std::endl; // Print sum
+
+  return 0;
+}
